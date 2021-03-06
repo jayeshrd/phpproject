26363eae1eb39d80af0ec568824f4afc097e139a
jayesh <jayrathore198@gmail.com>
2021-03-06 06:45:23 +0000

    i

--- php.sh
+++ php.sh
@@ -1,1 +1,8 @@
-test
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y
+apt install software-properties-common
+add-apt-repository ppa:ondrej/php
+apt install -y php7.4
+apt-cache search php7*
+apt install php7.4-mysql php7.4-curl php7.4-json php7.4-cgi php7.4-xsl php7.4-gd php7.4-php-fpm php7.4-php-gd php7.4-php-json php7.4-php-mbstring php7.4-php-mysqlnd php7.4-php-xml php7.4-php-xmlrpc php7.4-php-opcache -y
